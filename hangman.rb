42430e607e6103783fd0df34c75f56fb90d4aa0a
Utkarsh Dayal <uxd2486@rit.edu>
2022-09-28 16:59:41 +0530

    Added function that updates the display when a correct guess is made

--- hangman.rb
+++ hangman.rb
@@ -4,7 +4,7 @@ def pick_word
   words = IO.readlines('google-10000-english-no-swears.txt')
   chosen_word = nil
   while chosen_word.nil?
-    chosen_word = words[rand(words.length)]
+    chosen_word = words[rand(words.length)].strip
     chosen_word = nil if chosen_word.length > 12 || chosen_word.length < 5
   end
   chosen_word
@@ -13,4 +13,13 @@ end
 def print_display(count, displayed_word)
   puts "Turns remaining: #{count}"
   displayed_word.each_char { |char| print "#{char} " }
+end
+
+def update_display(letter, chosen_word, displayed_word)
+  index = chosen_word.index(letter)
+  until index.nil?
+    displayed_word[index] = letter
+    index = chosen_word.index(letter, index + 1)
+  end
+  displayed_word
 end
