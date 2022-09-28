fd8fcf475c520b7f483c0af4ccf04d4f4f608957
Utkarsh Dayal <uxd2486@rit.edu>
2022-09-28 16:35:08 +0530

    Added function to read dictionary file and choose an appropriate word

--- hangman.rb
+++ hangman.rb
@@ -1,1 +1,11 @@
+# frozen_string_literal: true
 
+def pick_word
+  words = IO.readlines('google-10000-english-no-swears.txt')
+  chosen_word = nil
+  while chosen_word.nil?
+    chosen_word = words[rand(words.length)]
+    chosen_word = nil if chosen_word.length > 12 || chosen_word.length < 5
+  end
+  chosen_word
+end
