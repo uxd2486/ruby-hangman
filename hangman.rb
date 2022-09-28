05df41b6a48b742cdf436156ee9b21b6bab04cc4
Utkarsh Dayal <uxd2486@rit.edu>
2022-09-28 22:39:51 +0530

    Added functionality to save the game in the game loop

--- hangman.rb
+++ hangman.rb
@@ -3,6 +3,8 @@
 require_relative 'game'
 
 def validate_input(string)
+  return string if string.eql? 'save'
+
   string = string.downcase
   return unless string.length == 1
   return unless /\A[a-z]\z/.match string
@@ -10,13 +12,23 @@ def validate_input(string)
   string
 end
 
+def save(game)
+  print 'Enter a name for the save file: '
+  name = gets.chomp
+  game.save(name)
+end
+
 def play
   puts 'Let\'s play Hangman!'
   game = Game.new
   until game.over?
     game.print_display
-    print 'Have a guess: '
+    print 'Have a guess(\'save\' to save the game): '
     guess = validate_input(gets.chomp)
+    if guess.eql? 'save'
+      save(game)
+      return
+    end
     if guess.nil?
       puts 'Invalid input'
       next
